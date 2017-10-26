b1ccc597b15c01e33d7543bdede558aeeece0e50
Anwesh43 <anweshthecool0@gmail.com>
2017-10-26 19:11:57 +0530

    creating SvgStage

--- svg-maker.js
+++ svg-maker.js
@@ -1,16 +1,44 @@
 document.createSVGElement = (tag) => {
     return document.createElementNS('http://www.w3.org/2000/svg',tag)
 }
-const svg = document.createSVGElement('svg')
-svg.setAttributeNS(null,"width","400")
-svg.setAttributeNS(null,"height","400")
-const text = document.createSVGElement('text')
-text.innerHTML = 'hello world'
-svg.appendChild(text)
-const circle = document.createSVGElement('circle')
-circle.setAttributeNS(null,'cx',200)
-circle.setAttributeNS(null,'cy',200)
-circle.setAttributeNS(null,'r',150)
-circle.setAttributeNS(null,'fill','red')
-svg.appendChild(circle)
-document.body.appendChild(svg)
+class SVGStageUtil {
+    static createAttributes(elem,map) {
+        for(var key in map) {
+            elem.setAttributeNS(null,key,map[key])
+        }
+    }
+}
+class SVGStage {
+    constructor(w,h) {
+        this.svg = document.createSVGElement('svg')
+        this.createStage(w,h)
+        this.fillColor = 'black'
+        this.strokeColor = 'black'
+    }
+    createStage(w,h) {
+        const dimensionsMap = {"width":w,"height":h}
+        SVGStageUtil.createAttributes(this.svg,dimensionsMap)
+        document.body.appendChild(this.svg)
+    }
+    setFillStyle(color) {
+        this.fillColor = color
+    }
+    setStrokeStyle(color) {
+        this.strokeColor = color
+    }
+    fillCircle(cx,cy,r) {
+        const fill = this.fillColor
+        const circleMap = {cx,cy,r,fill}
+        const circle = document.createSVGElement('circle')
+        SVGStageUtil.createAttributes(circle,circleMap)
+        this.svg.appendChild(circle)
+    }
+    strokeCircle(cx,cy,r) {
+        const stroke = this.strokeColor
+        const fill = 'none'
+        const circleMap = {cx,cy,r,stroke,fill}
+        const circle = document.createSVGElement('circle')
+        SVGStageUtil.createAttributes(circle,circleMap)
+        this.svg.appendChild(circle)
+    }
+}
